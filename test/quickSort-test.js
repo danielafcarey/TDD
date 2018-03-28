c09dbd76b04389c1f94b83f82827cf32a3e91ca2
Daniela Carey <danielafcarey@gmail.com>
2018-03-28 11:01:30 -0600

    Implement quickSort and pass tests
    Does not pass for 90k-length array

--- test/quickSort-test.js
+++ test/quickSort-test.js
@@ -10,23 +10,31 @@ describe('quickSort', () => {
   it('should sort an unsorted array', () => {
     let unsorted = arrayGenerator(5);
 
-    quickSort(unsorted);
+    unsorted = quickSort(unsorted);
 
     expect(unsorted).to.be.sorted();
   })
 
-  it.skip('should sort an unsorted array with 1000 items', () => {
+  it('should sort an unsorted array with 1,000 items', () => {
     let unsorted = arrayGenerator(1000);
 
-    quickSort(unsorted);
+    unsorted = quickSort(unsorted);
 
     expect(unsorted).to.be.sorted();
   })
 
-  it.skip('should sort an unsorted array with 10000 items', () => {
+  it('should sort an unsorted array with 10,000 items', () => {
     let unsorted = arrayGenerator(10000);
 
-    quickSort(unsorted);
+    unsorted = quickSort(unsorted);
+
+    expect(unsorted).to.be.sorted();
+  })
+
+  it('should sort an unsorted array with 90,000 items', () => {
+    let unsorted = arrayGenerator(90000);
+
+    unsorted = quickSort(unsorted);
 
     expect(unsorted).to.be.sorted();
   })
@@ -36,7 +44,7 @@ describe('quickSort', () => {
   it('should sort an array with multiple of the same elements in it', () => {
     let unsorted = [1, 6, 6, 7, 3, 4]
 
-    quickSort(unsorted);
+    unsorted = quickSort(unsorted);
 
     expect(unsorted).to.be.sorted();
   })
