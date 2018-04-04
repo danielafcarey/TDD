2aa702094f276f4feffebf3bc7c53d90f43dd156
Daniela Carey <danielafcarey@gmail.com>
2018-04-03 19:37:59 -0600

    Implement suggest method and update tests
    Add test to account for duplicate word insertion

--- scripts/Trie.js
+++ scripts/Trie.js
@@ -1,35 +1,29 @@
-import Node from './Node.js'
+const Node = require('./Node.js')
 
-export default class Trie {
+class Trie {
   constructor() {
     this.root = new Node();
     this.wordCount = 0;
+    this.suggestions = [];
   }
 
   insert(word) {
     let currentNode = this.root;
 
     for (let i = 0; i < word.length; i++) {
-      // if the current node children obj contains a key of the letter 
-      if (currentNode.children[word[i]]) {
-        // move the current node to that letter obj value
-        currentNode = currentNode.children[word[i]];
-
-      // else it is not yet in the currentNode.children, so add it
-      } else {
-        // currentNode Obj key with letter: {new node obj}
+      if (!currentNode.children[word[i]]) {
         currentNode.children[word[i]] = new Node(word[i]);
-        // and move the current node to that new obj
-        currentNode = currentNode.children[word[i]];
       }
-
+      currentNode = currentNode.children[word[i]];
     }
-    //after looping through whole word, you're at the end of the word
-    currentNode.endOfWord = word;
 
-    //change this.wordCount when it breaks out of for loop
-    this.wordCount++;
-
+    if (currentNode.endOfWord) {
+      return;
+    } else {
+      currentNode.endOfWord = true;
+      this.wordCount++;
+    }
+    
   }
 
   count() {
@@ -37,24 +31,43 @@ export default class Trie {
   }
 
   suggest(prefix) {
+    this.suggestions = [];
     let currentNode = this.root;
-    const suggestions = [];
 
-    //traverse through the tree --> will go all the way down the left side first. 
-    // --> need to find a way to set the counter at a split
+    for (let i = 0; i < prefix.length; i++) {
+      if (currentNode.children[prefix[i]]) {
+        currentNode = currentNode.children[prefix[i]]
+      } else {
+        return null;
+      }
+    }
 
-    //OR can traverse with BFS that searches each value in the .children property to see if it.endOfWord is null, pushes if it so, then does the same for the next item in the
-
-    // if (currentNode.endOfWord) {
-    //   suggestions.push(currentNode.endOfWord)
-    // }
-
+    this.findWordSuggestions(currentNode, prefix);
+    return this.suggestions;
   }
+
+  findWordSuggestions(startNode, prefix) {
+    Object.keys(startNode.children).forEach(childData => { 
+      let currentNode = startNode.children[childData];
+
+      if (currentNode.endOfWord) {
+        this.suggestions.push(prefix + childData)
+      } 
+
+      return this.findWordSuggestions(currentNode, prefix + childData)
+    })
+  } 
 
   populate(dataSet) {
-    dataSet.forEach(data => {
-      this.insert(data);
-    });
+    dataSet.forEach(data => this.insert(data));
   }
 
+  //find() {}
+
 }
+
+
+
+
+
+module.exports = Trie;
